e303cb355fbfe1e9a683480c54109700cf054ee7
Lucia Gonzalez Moscoso <luciagonzalezmoscoso@gmail.com>
2018-04-13 13:54:11 -0700

    Add the player

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed, sprite) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -24,11 +26,56 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y, sprite) {
+    this.x = 202;
+    this.y = 400;
+    this.sprite = 'images/char-horn-girl.png';
+};
 
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+
+};
+
+//Draw the player on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyCode) {
+    switch (keyCode) {
+      case 'left':
+        if(this.x > 100) {
+          this.x -= 101;
+        }
+        break;
+      case 'up':
+        if(this.y > 0) {
+          this.y -= 83;
+        }
+        break;
+      case 'right':
+        if(this.x >= 0 && this.x < 400) {
+          this.x += 101;
+        }
+        break;
+      case 'down':
+        if(this.y < 400) {
+          this.y += 83;
+        }
+        break;
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
+let enemy1 = new Enemy(100, 200, 70);
+allEnemies.push(enemy1);
+
 // Place the player object in a variable called player
+var player = new Player();
 
 
 
